1468cf15ab993f44296532f7d6b2ab9b2c6069e1
Arun Kumar <arunkumar.aqm@gmail.com>
2019-11-04 12:55:20 +0530

    Add delete and make some significant changes (Untested)

--- AvlTree.c
+++ AvlTree.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+//https://github.com/amitbansal7/Data-Structures-and-Algorithms/blob/master/8.AVL-tree/AVLTree.c
+
 struct Node; // A forward declaration
 typedef struct Node* NodePointer;
 
@@ -13,12 +15,30 @@ typedef struct Node* NodePointer;
 		ptr = NULL;        \
 }
 
+int inline max(int one,int two)
+{
+ 	return (one > two)? one: two;
+}
+
+int inline height(NodePointer node)
+{
+	if (node == NULL)
+		return 0;
+
+ 	return node->height;
+}
+
+int inline balance(NodePointer parent){
+
+	if (parent == NULL) return 0;
+	else return height(parent->left) - height(parent->right);
+}
+
 struct Node{
 
 	int value;
-	NodePointer parent, left, right; //parent might be redundant
-	short int balance;
-	int height; // height might be redundant
+	NodePointer left, right; 
+	int height; 
 };
 
 typedef struct Tree{
@@ -73,15 +93,13 @@ void destructor(Tree* tree){ // Modifies root node
 }
 
 // Returns the address of a newly created node
-NodePointer create_node(int value, 
-						NodePointer parent){
+NodePointer create_node(int value){
 
 	NodePointer new_node = malloc(sizeof *new_node);
 	new_node->value = value;
-	new_node->parent = parent;
 	new_node->left = new_node->right = NULL;
 	new_node->balance = 0;
-	new_node->height = 0;
+	new_node->height = 1;
 	return new_node;
 }
 
@@ -95,7 +113,7 @@ void insert_into_tree(int value, NodePointer parent){ // Modifies non-root nodes
 
 		if (parent->left == NULL){
 
-			parent->left = create_node(value, parent);
+			parent->left = create_node(value);
 		}
 		else{
 
@@ -107,7 +125,7 @@ void insert_into_tree(int value, NodePointer parent){ // Modifies non-root nodes
 
 		if (parent->right == NULL){
 
-			parent->right = create_node(value, parent);
+			parent->right = create_node(value);
 		}
 		else{
 
@@ -123,7 +141,7 @@ void read_tree(Tree* tree){ // Modifies root node
 	// Read the root node first
 	{
 		scanf("%d %c", &value, &choice);
-		tree->root = create_node(value, NULL);
+		tree->root = create_node(value);
 	}
 
 	// If choice is yes, read more elements
@@ -177,6 +195,10 @@ NodePointer rotate_left(NodePointer node){
 	NodePointer other = node->right;
 	node->right = other->left;
 	other->left = node;
+	
+	node->height = max(height(node->left), height(node->right)) + 1;
+	other->height = max(height(other->left), height(other->right)) + 1;
+	
 	return other; 
 }
 
@@ -185,6 +207,10 @@ NodePointer rotate_right(NodePointer node){
 	NodePointer other = node->left;
 	node->left = other->right;
 	other->right = node;
+	
+	node->height = max(height(node->left), height(node->right)) + 1;
+	other->height = max(height(other->left), height(other->right)) + 1;
+	
 	return other; 
 }
 
@@ -200,6 +226,107 @@ NodePointer rotate_right_left(NodePointer node){
 	return rotate_left(node);
 }
 //---------------------------------------
+/*
+// Returns the leftmost node in the right subtree - which would be the node with minimum value
+NodePointer find_minimum(NodePointer node){
+	
+	while(node->left != NULL)
+		node = node->left;
+	return node;
+}
+*/
+// Returns parent of minimum element
+NodePointer get_parent_of_minimum(NodePointer parent){
+
+	if (parent->left->left){
+		return get_parent_of_minimum(NodePointer parent->left);
+	}
+	else{
+		return parent;
+	}
+}
+
+NodePointer delete_node(NodePointer root, int item){ // Modifies non-root nodes and returns modified root
+	
+	// Empty subtree
+	if (root == NULL)
+		return NULL;
+
+	if (data < root->value)
+		root->left = delete_node(root->left, item);
+
+	else if (data > root->value)
+		root->right = delete_node(root->right, item);
+
+	else{
+
+		if (root->right == NULL){
+			
+			// Node to be deleted is a leaf
+			if (root->left == NULL){
+			
+				free(root);
+				root = NULL;
+				return NULL;
+			}
+			
+			// Node to be deleted has only left child
+			else{
+				NodePointer temp = root->left;
+				root = root->left;
+				free(temp);
+			}
+		}
+		
+		// Node to be deleted has only right child
+		else if (root->left == NULL){
+	
+			{
+				NodePointer temp = root->right;
+				root = root->right;
+				free(temp);
+			}
+		}
+
+		// Node to be deleted has both children
+		else
+		{
+			// The minimum value is copied onto 
+			NodePointer temp = get_parent_of_minimum(root->right);
+			root->data = temp->data;
+			root->right = delete_node(root->right, temp->data);
+		}
+	}
+	
+	root->height = 1 + max(height(root->left), height(root->right));
+
+	int balance = calc_balance(root);
+
+	//Left Left Case
+	if (balance > 1){
+		
+		if (calc_balance(root->left) < 0)
+			return rotate_left_right(root);
+		else
+			return rotate_right(root);
+	}
+	
+	else if (balance < -1){
+		
+		if (calc_balance(root->right) > 0)
+			return rotate_right_left(root);
+		else
+			return rotate_left(root);
+	}
+	
+	return root;
+}
+
+void delete_from_tree(TreePointer tree, int item){
+
+	tree->root = delete_node(tree->root, item);
+}
+
 
 // Driver function
 void main(){
