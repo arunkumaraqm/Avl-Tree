7bc117074907b968747022df91311465ac68bf6a
Arun Kumar <arunkumar.aqm@gmail.com>
2019-11-06 21:31:11 +0530

    Move some stuff to AvlTree.h; Add code partitions

--- AvlTree.c
+++ AvlTree.c
@@ -2,62 +2,18 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include "AvlTree.h"
 
-//https://github.com/amitbansal7/Data-Structures-and-Algorithms/blob/master/8.AVL-tree/AVLTree.c
+//https://github.com/amitbansal7/value-Structures-and-Algorithms/blob/master/8.AVL-tree/AVLTree.c
 
-struct Node; // A forward declaration
-typedef struct Node* NodePointer;
 
-// Macro to free any pointer and assign it to NULL
-#define free_and_null(ptr){\
-				           \
-		free(ptr);         \
-		ptr = NULL;        \
-}
-
-int inline max(int one,int two)
-{
- 	return (one > two)? one: two;
-}
-
-int inline height(NodePointer node)
-{
-	if (node == NULL)
-		return 0;
-
- 	return node->height;
-}
-
-int inline balance(NodePointer parent){
-
-	if (parent == NULL) return 0;
-	else return height(parent->left) - height(parent->right);
-}
-
-struct Node{
-
-	int value;
-	NodePointer left, right; 
-	int height; 
-};
-
-typedef struct Tree{
-
-	NodePointer root;
-}Tree;
+//***********************************************
+// CONSTRUCTOR AND DESTRUCTOR
 
 // Should be called after creating a Tree object
 void constructor(Tree* tree){ // Modifies root node
 
 	tree->root = NULL;
-}
-
-
-// Representation of a node
-void repr_node(NodePointer node){ // Temparory function
-
-	printf("[%d, %p, %p, %p, %d, %d]\n", 
-		     node->value, node->parent, node->left, node->right, node->balance, node->height);
 }
 
 // This function does not kill a parent, but kills the children. 
@@ -92,13 +48,93 @@ void destructor(Tree* tree){ // Modifies root node
 	}
 }
 
+//***********************************************
+// DISPLAY FUNCTIONALITY
+
+// Printing with tabspaces
+void recursive_print_node(NodePointer parent, int noof_tabs){
+
+// parent->value
+//     parent->left->value
+//         parent->left->...->value
+//             						... Null
+//         parent->right->...->value
+//             						... Null
+//     parent->right->value
+//         parent->right->...->value
+//             						... Null
+//         parent->right->...->value
+//             						... Null
+
+	for (int i = 0; i < noof_tabs; printf("    "), ++i);
+
+	if (parent == NULL){
+
+		printf("Null\n");
+		return;
+	}
+
+	printf("%d\n", parent->value);
+	recursive_print_node(parent->left, noof_tabs + 1);
+	recursive_print_node(parent->right, noof_tabs + 1);
+}
+
+void print_tree(Tree *tree){
+
+	recursive_print_node(tree->root, 0); // 0 - no indentation in output for root node
+}
+
+//***********************************************
+// ROTATION FUNCTIONS
+
+// Pass in the old root of the subtree
+// Returns the new root of the subtree
+
+NodePointer rotate_left(NodePointer node){
+
+	NodePointer other = node->right;
+	node->right = other->left;
+	other->left = node;
+	
+	node->height = max(height(node->left), height(node->right)) + 1;
+	other->height = max(height(other->left), height(other->right)) + 1;
+	
+	return other; 
+}
+
+NodePointer rotate_right(NodePointer node){
+
+	NodePointer other = node->left;
+	node->left = other->right;
+	other->right = node;
+	
+	node->height = max(height(node->left), height(node->right)) + 1;
+	other->height = max(height(other->left), height(other->right)) + 1;
+	
+	return other; 
+}
+
+NodePointer rotate_left_right(NodePointer node){
+
+	node->left = rotate_left(node->left);
+	return rotate_right(node);
+}
+
+NodePointer rotate_right_left(NodePointer node){
+
+	node->right = rotate_right(node->right);
+	return rotate_left(node);
+}
+
+//***********************************************
+// INSERTION FUNCTIONALITY
+
 // Returns the address of a newly created node
 NodePointer create_node(int value){
 
 	NodePointer new_node = malloc(sizeof *new_node);
 	new_node->value = value;
 	new_node->left = new_node->right = NULL;
-	new_node->balance = 0;
 	new_node->height = 1;
 	return new_node;
 }
@@ -152,94 +188,14 @@ void read_tree(Tree* tree){ // Modifies root node
 	}
 }
 
-// Printing with tabspaces
-void recursive_print_node(NodePointer parent, int noof_tabs){
+//***********************************************
+// DELETION FUNCTIONALITY
 
-// parent->value
-//     parent->left->value
-//         parent->left->...->value
-//             						... Null
-//         parent->right->...->value
-//             						... Null
-//     parent->right->value
-//         parent->right->...->value
-//             						... Null
-//         parent->right->...->value
-//             						... Null
-
-	for (int i = 0; i < noof_tabs; printf("    "), ++i);
-
-	if (parent == NULL){
-
-		printf("Null\n");
-		return;
-	}
-
-	printf("%d\n", parent->value);
-	recursive_print_node(parent->left, noof_tabs + 1);
-	recursive_print_node(parent->right, noof_tabs + 1);
-}
-
-void print_tree(Tree *tree){
-
-	recursive_print_node(tree->root, 0); // 0 - no indentation in output for root node
-}
-
-//----------------------------------------
-// The four rotate functions
-// Pass in the old root of the subtree
-// Returns the new root of the subtree
-
-NodePointer rotate_left(NodePointer node){
-
-	NodePointer other = node->right;
-	node->right = other->left;
-	other->left = node;
-	
-	node->height = max(height(node->left), height(node->right)) + 1;
-	other->height = max(height(other->left), height(other->right)) + 1;
-	
-	return other; 
-}
-
-NodePointer rotate_right(NodePointer node){
-
-	NodePointer other = node->left;
-	node->left = other->right;
-	other->right = node;
-	
-	node->height = max(height(node->left), height(node->right)) + 1;
-	other->height = max(height(other->left), height(other->right)) + 1;
-	
-	return other; 
-}
-
-NodePointer rotate_left_right(NodePointer node){
-
-	node->left = rotate_left(node->left);
-	return rotate_right(node);
-}
-
-NodePointer rotate_right_left(NodePointer node){
-
-	node->right = rotate_right(node->right);
-	return rotate_left(node);
-}
-//---------------------------------------
-/*
-// Returns the leftmost node in the right subtree - which would be the node with minimum value
-NodePointer find_minimum(NodePointer node){
-	
-	while(node->left != NULL)
-		node = node->left;
-	return node;
-}
-*/
 // Returns parent of minimum element
 NodePointer get_parent_of_minimum(NodePointer parent){
 
 	if (parent->left->left){
-		return get_parent_of_minimum(NodePointer parent->left);
+		return get_parent_of_minimum(parent->left);
 	}
 	else{
 		return parent;
@@ -252,10 +208,10 @@ NodePointer delete_node(NodePointer root, int item){ // Modifies non-root nodes 
 	if (root == NULL)
 		return NULL;
 
-	if (data < root->value)
+	if (item < root->value)
 		root->left = delete_node(root->left, item);
 
-	else if (data > root->value)
+	else if (item > root->value)
 		root->right = delete_node(root->right, item);
 
 	else{
@@ -293,8 +249,8 @@ NodePointer delete_node(NodePointer root, int item){ // Modifies non-root nodes 
 		{
 			// The minimum value is copied onto 
 			NodePointer temp = get_parent_of_minimum(root->right);
-			root->data = temp->data;
-			root->right = delete_node(root->right, temp->data);
+			root->value = temp->value;
+			root->right = delete_node(root->right, temp->value);
 		}
 	}
 	
@@ -327,8 +283,9 @@ void delete_from_tree(TreePointer tree, int item){
 	tree->root = delete_node(tree->root, item);
 }
 
+//***********************************************
+// MAIN
 
-// Driver function
 void main(){
 
 	Tree* tree = malloc(sizeof *tree);
@@ -341,3 +298,4 @@ void main(){
 	free_and_null(tree);
 }
 
+//***********************************************
